3bbd8d2b0e0c929b35f1013ec5e61262ff58d12f
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-25 14:57:56 +0800

    feat: add row actions w dropdown

--- src/app/leaderboard/columns.tsx
+++ src/app/leaderboard/columns.tsx
@@ -1,8 +1,17 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -98,4 +107,43 @@ export const columns: ColumnDef<Player>[] = [
             )
         },
     },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const player = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end" className="bg-white">
+                        <DropdownMenuLabel>{player.name}</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(player.name)} // replace `player.name` with the player's ID #
+                            className="cursor-pointer"
+                        >
+                            Copy player ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                            onClick={() => { }} // go to player profile url
+                            className="cursor-pointer"
+                        >
+                            View player profile
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            onClick={() => { }} // go to block explorer url of the player address
+                            className="cursor-pointer"
+                        >
+                            View player transactions
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    }
 ]
