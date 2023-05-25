c20bf80715720265824fb795ff59b29191ef724f
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-25 13:31:13 +0800

    fix: player object types in LeaderboardTable

--- src/app/leaderboard/LeaderboardTable.tsx
+++ src/app/leaderboard/LeaderboardTable.tsx
@@ -8,86 +8,95 @@ import {
     TableRow,
 } from "@/components/ui/table"
 
-const players = [
+type Player = {
+    name: string
+    wins: number
+    losses: number
+    winRatio: number
+    strength: number
+    totalMatches: number
+}
+
+const players: Player[] = [
     {
         name: "Jura",
-        wins: "655",
-        losses: "16",
-        winRatio: "97.6%",
-        strength: "1234",
-        totalMatches: "671",
+        wins: 655,
+        losses: 16,
+        winRatio: 0.976,
+        strength: 1234,
+        totalMatches: 671,
     },
     {
         name: "Elons",
-        wins: "255",
-        losses: "46",
-        winRatio: "84.7%",
-        strength: "149",
-        totalMatches: "301",
+        wins: 255,
+        losses: 46,
+        winRatio: 0.847,
+        strength: 149,
+        totalMatches: 301,
     },
     {
         name: "pjotrek",
-        wins: "222",
-        losses: "31",
-        winRatio: "87.7%",
-        strength: "277",
-        totalMatches: "253",
+        wins: 222,
+        losses: 31,
+        winRatio: 0.877,
+        strength: 277,
+        totalMatches: 253,
     },
     {
         name: "Gauz",
-        wins: "205",
-        losses: "34",
-        winRatio: "85.8%",
-        strength: "256",
-        totalMatches: "239",
+        wins: 205,
+        losses: 34,
+        winRatio: 0.858,
+        strength: 256,
+        totalMatches: 239,
     },
     {
         name: "Integer",
-        wins: "142",
-        losses: "8",
-        winRatio: "94.7%",
-        strength: "333",
-        totalMatches: "150",
+        wins: 142,
+        losses: 8,
+        winRatio: 0.946,
+        strength: 333,
+        totalMatches: 150,
     },
     {
         name: "Spartacus",
-        wins: "106",
-        losses: "46",
-        winRatio: "69.7%",
-        strength: "123",
-        totalMatches: "152",
+        wins: 106,
+        losses: 46,
+        winRatio: 0.696,
+        strength: 123,
+        totalMatches: 152,
     },
     {
         name: "SOV",
-        wins: "67",
-        losses: "0",
-        winRatio: "100%",
-        strength: "888",
-        totalMatches: "67",
+        wins: 67,
+        losses: 0,
+        winRatio: 1,
+        strength: 888,
+        totalMatches: 67,
     },
     {
         name: "paul585",
-        wins: "66",
-        losses: "36",
-        winRatio: "64.7%",
-        strength: "98",
-        totalMatches: "102",
+        wins: 66,
+        losses: 36,
+        winRatio: 0.647,
+        strength: 98,
+        totalMatches: 102,
     },
     {
         name: "Rektpleb",
-        wins: "64",
-        losses: "2",
-        winRatio: "97.0%",
-        strength: "555",
-        totalMatches: "66",
+        wins: 64,
+        losses: 2,
+        winRatio: 0.970,
+        strength: 555,
+        totalMatches: 66,
     },
     {
         name: "DAWID",
-        wins: "63",
-        losses: "31",
-        winRatio: "67.0%",
-        strength: "87",
-        totalMatches: "94",
+        wins: 63,
+        losses: 31,
+        winRatio: 0.670,
+        strength: 87,
+        totalMatches: 94,
     },
 ]
 
@@ -111,7 +120,7 @@ export default function LeaderboardTable() {
                         <TableCell className="font-medium">{player.name}</TableCell>
                         <TableCell>{player.wins}</TableCell>
                         <TableCell>{player.losses}</TableCell>
-                        <TableCell>{player.winRatio}</TableCell>
+                        <TableCell>{player.winRatio * 100}%</TableCell>
                         <TableCell>{player.strength}</TableCell>
                         <TableCell className="text-right">{player.totalMatches}</TableCell>
                     </TableRow>
