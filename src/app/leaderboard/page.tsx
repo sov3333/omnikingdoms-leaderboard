d3d9f621c3bdeabcffb66b63d2f41ed841c6f4c9
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-25 19:00:44 +0800

    fix: moved getData function to inside component

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -3,27 +3,27 @@ import { DataTable } from "./data-table"
 
 const siteUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000'
 
-async function getData(): Promise<Player[]> {
-    // Fetch data from API here.
-    try {
-        const players = await fetch(`${siteUrl}/api`, { method: "GET" })
-
-        if (players.status === 404) {
-            console.log("Error getting players")
-            return []
-        }
-
-        const playersJson = players.json()
-        return playersJson
-
-    } catch (error) {
-        console.log(error)
-        return []
-    }
-}
-
 export default async function Leaderboard() {
     const data = await getData()
+
+    async function getData(): Promise<Player[]> {
+        // Fetch data from API here.
+        try {
+            const players = await fetch(`${siteUrl}/api`, { method: "GET" })
+
+            if (players.status !== 200) {
+                console.log("Error getting players")
+                return []
+            } else {
+                const playersJson = players.json()
+                return playersJson
+            }
+
+        } catch (error) {
+            console.log(error)
+            return []
+        }
+    }
 
     return (
         <div className="container mx-auto py-[1rem]">
